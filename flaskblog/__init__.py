4b820d229a9babaf4d29534d4ea20756f570238b
Suhaf Butt <suhaf.azam@gmail.com>
2020-06-18 04:24:55 +0200

    Reset Password Module Added

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f75a5bc66c3a9954dd34809272ef27ff'
@@ -11,5 +12,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'Add Credentials Here'
+app.config['MAIL_PASSWORD'] = 'Add Credentials Here'
+mail = Mail(app)
 
 from flaskblog import routes
